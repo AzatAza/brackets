14964a3bc18a8cc12727c26e09a34beae085f4ea
AzatAza <78497811+AzatAza@users.noreply.github.com>
2021-03-11 12:54:27 +0500

    feat: "add implement"

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+    let open = [],
+        close = [],
+        stack = [];
+
+    bracketsConfig.forEach((i) => {
+    open.push(i[0]);
+    close.push(i[1]);
+  })
+
+  for (let i = 0; i < str.length; i++) {
+    if (open.includes(str[i]) && close.includes(str[i]) && stack[stack.length - 1] == str[i]) {
+      stack.pop()
+    } else if (open.includes(str[i])) {
+      stack.push(str[i])
+    } else if (close.indexOf(str[i]) != -1 && stack[stack.length - 1] == open[close.indexOf(str[i])]) {
+      stack.pop()
+    } else {
+      return false
+    }
+  }
+
+  return stack.length == 0 ? true : false;
 }
